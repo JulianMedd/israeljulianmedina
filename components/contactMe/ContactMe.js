dcbc68aab8046cd45777829628d3fac3ccc294c4
julian medina <julian.medina7@hotmail.com.ar>
2022-01-18 17:44:29 -0300

    adding new component contact, adding the formik librery, fixing somes package

---
+++ components/contactMe/ContactMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LayoutSection from '../layout/LayoutSection';
+
+const ContactMe = () => {
+    return (
+        <LayoutSection title='CONTACT ME'>
+            <div>
+                
+            </div>
+
+        </LayoutSection>
+    )
+}
+
+export default ContactMe;
---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactMe from '../components/contactMe/ContactMe';
+import NavBar from '../components/menu';
+
+const Contact = () => {
+    return (
+        <main>
+            <NavBar/>
+            <ContactMe/>  
+        </main>
+    )
+}
+
+export default Contact;
